d346e53049eebbc83b94d93822239eae89125d24
jtegtmeier <jcbtegtmeier@gmail.com>
2017-03-16 17:48:11 -0700

    added fork ribbon

--- src/App.js
+++ src/App.js
@@ -9,10 +9,11 @@ class App extends Component {
     return (
       <div className="app">
         <div>
-          <h2>Syncing state with&nbsp;
+          <h1>React Comment Hub</h1>
+          <h2>Syncing app state with&nbsp;
             <a href="https://facebook.github.io/react/">React</a> and&nbsp;
-            <a href="https://firebase.google.com/">Firebase</a> using&nbsp;
-            <a href="https://github.com/tylermcginnis/re-base">re&#8209;base</a>
+            <a href="https://firebase.google.com/">Firebase</a> using the&nbsp;
+            <a href="https://github.com/tylermcginnis/re-base">re&#8209;base</a> library.
           </h2>
           <h4>By  <a href="https://github.com/jtegtmeier">Jacob Tegtmier</a></h4>
         </div>
